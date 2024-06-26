82c9125f9d6d6bc4e49ed6fb69444fdf6ca26f32
Mona Lisa <roldansebasdaza@gmail.com>
2024-06-25 20:00:52 -0500

    25/06/2024

--- hotel/hotel/views.py
+++ hotel/hotel/views.py
@@ -7,23 +7,3 @@ def index(request):
 
 def contacto(request):
     return render(request, 'contacto.html')
-
-def habitacion(request):
-    return render(request, 'habitacion.html')
-
-def paquetes(request):
-    objpaquete = paquetes.objects.all()
-
-    contex = {
-        'paq': objpaquete,
-    }
-    return render(request, 'paquetes.html', contex)
-
-
-def servicios(request):
-    objservicios = servicio.objects.all()
-
-    context = {
-        'serv': objservicios,
-    }
-    return render(request, 'servicios.html', context)
